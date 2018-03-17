830f8e6762bed8f05bd5123d15649bc9db9d84ad
AndrianCode <anpojda@gmail.com>
2018-03-17 17:33:13 +0200

    image getter updated

--- get-all-images-from-web.py
+++ get-all-images-from-web.py
@@ -1,8 +1,13 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
-webs = ["https://en.wikipedia.org/wiki/Albert_Einstein"]
-for web in webs:
+from PIL import ImageEnhance
+import base64
+
+def get_all_images_from_web(web):
+	images = []
 	page = BeautifulSoup(urllib2.urlopen(web))
-	images = page.findAll('img')
-	for image in images:
-		print(image['src'])
+	links = page.findAll('img')
+	for link in links:
+		image = base64.b64encode(link.get('src'))
+		images.append(image)
+	return images
