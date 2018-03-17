cbd62692be353a0221131370f8c05d655683eb27
AndrianCode <anpojda@gmail.com>
2018-03-17 17:49:16 +0200

    correct binary convertion

--- get-all-images-from-web.py
+++ get-all-images-from-web.py
@@ -2,12 +2,17 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 from PIL import ImageEnhance
 import base64
+import urllib
+import requests
 
 def get_all_images_from_web(web):
 	images = []
 	page = BeautifulSoup(urllib2.urlopen(web))
 	links = page.findAll('img')
 	for link in links:
-		image = base64.b64encode(link.get('src'))
-		images.append(image)
+		try:
+			image = base64.b64encode(requests.get("http:" + link.get('src')).content)
+			images.append(image)
+		except requests.exceptions.MissingSchema:
+			pass
 	return images
